8e833f04f32dbf8023babd9013635a53b674c51f
ryankielty <rpkielty@gmail.com>
2016-12-26 13:02:48 -0600

    updated to login

---
+++ src/com/company/User.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by ryankielty on 12/23/16.
+ */
+public class User {
+    String userName;
+    String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
